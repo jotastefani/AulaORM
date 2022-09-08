37ab8568a0a859ba973753ab72dd82553ce4dc27
J.Stefani <jota.stefani@hotmail.com>
2022-09-08 07:51:42 -0300

    Aula2 ORM

--- aulaorm/src/main/java/com/devsuperior/aulaorm/AulaormApplication.java
+++ aulaorm/src/main/java/com/devsuperior/aulaorm/AulaormApplication.java
@@ -1,14 +1,20 @@
 package com.devsuperior.aulaorm;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devsuperior.aulaorm.entities.Department;
 import com.devsuperior.aulaorm.entities.Employee;
+import com.devsuperior.aulaorm.repositories.EmployeeRepository;
 
 @SpringBootApplication
 public class AulaormApplication implements CommandLineRunner {
+	
+	@Autowired
+	private EmployeeRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaormApplication.class, args);
@@ -17,12 +23,11 @@ public class AulaormApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Department d1 = new Department(1L, "Vendas");
-		Department d2 = new Department(2L, "Marketing");
+		List<Employee> result = repository.findAll();
 		
-		Employee e1 = new Employee(1L, "Maria", 6000.0, d1);
-		
-		System.out.println(e1);
+		for(Employee emp : result) {
+			System.out.println(emp);
+		}
 	}
 	
 	
